ce2ddcf9bd9abcb94358f7dab5e1d35ea7f08aeb
Aravind <gundettiaravind21@gmail.com>
2025-06-28 21:46:55 +0530

    changed the port

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
-
-app.listen(3000,()=>{
+const port = process.env.PORT || 4000 
+app.listen(port,()=>{
     console.log("listening at 3000");
 })
